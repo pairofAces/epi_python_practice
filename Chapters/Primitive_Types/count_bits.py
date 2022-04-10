ae4ec52721a55946ffcbab5d05627c438f1cbfab
pairofAces <karanchauhan16@gmail.com>
2022-04-10 17:58:53 -0400

    included exlanation about time complexity

--- Chapters/Primitive_Types/count_bits.py
+++ Chapters/Primitive_Types/count_bits.py
@@ -6,3 +6,7 @@ def count_bits(x: int) -> int:
         num_bits += x & 1
         x >>= 1
     return num_bits
+
+# Time complexity:
+    # Since the program is performing O(1) computation per bit, the time complexity
+    # is O(n), where (n) is the number of bits required to represent an integer
